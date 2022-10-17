eabdf73ec430c2124de87124ab65e92de4fa1d45
EmmanuelDELGRANGE <delgrangeemmanuel@gmail.com>
2022-10-17 11:33:25 +0200

    Create project and basic components
    First commit:
    - Create main file
    - Init functions
    - Readme.md with explanations of project structure prepare and "how to play" section

---
+++ lib/init.h
@@ -0,0 +1,18 @@
+/*
+This library is used to define initialize every main functions and variables of the game
+
+Use this to change the core components of the program, or to define the main
+functions that are use to initialize elements
+*/
+/*
+#include<stdio.h>
+#include<stdlib.h>
+
+
+#define CELL_ALIVE 'o'
+#define CELL_DEAD 'x'
+#define CELL_EMPTY ' '
+
+#define GRIDSIZE 20
+#define GRID [GRIDSIZE][GRIDSIZE]
+*/
---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"./lib/init.h"
+
+
+
+int main(){
+    //initGame();
+    
+
+    return 0;
+}
